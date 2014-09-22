ff3661bbaa4e427c640329019adc7112fc135bc8
LAB4-PC01 <LAB4-PC01@LAB4-PC01-PC>
2014-09-22 14:03:10 -0300

    Projeto recompilado no CodeLite

--- EBook/main.cpp
+++ EBook/main.cpp
@@ -1,7 +1,41 @@
-#include <stdio.h>
+#include <iostream>
+#include <list>
+#include <string>
+#include <conio.h>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
+#include "Livro.h"
+#include "EBook.h"
+#include "Anotacao.h"
+
+using namespace std;
+
+int main(void){
+
+	list<EBook> eBooks;
+	EBook eBook01(Livro("Livro01", "Editora01", "Autor01", 9781214558875, 152));
+	eBooks.push_back(EBook(eBook01));
+	//eBooks.emplace_back(eBook01);
+	eBooks.push_back(EBook(Livro("Livro02", "Editora01", "Autor01", 9781234567890, 25), "Leitor01"));
+	//eBooks.emplace_back(Livro("Livro02", "Editora01", "Autor01", 9781234567890, 25), "Leitor01");
+
+	
+	cout << "Criou anotacao: " << ((eBooks.front().novaAnotacao(151, "Lorem Ipsum")) ? "true" : "false") << "\n";
+	cout << "Criou anotacao: " << ((eBooks.front().novaAnotacao(132, "asbudabsd")) ? "true" : "false") << "\n";
+	cout << "Criou anotacao: " << ((eBooks.front().novaAnotacao(545, "cacacabgbgb")) ? "true" : "false") << "\n";
+	cout << "Criou anotacao: " << ((eBooks.front().novaAnotacao(12, "Blablablabla")) ? "true" : "false") << "\n";
+
+	string aa = "asjnas";
+	int i = 0;
+	for (list<EBook>::iterator it = eBooks.begin(); it != eBooks.end(); ++it){
+		i++;
+		cout << "Livro Num: " << i << endl;
+
+		eBook01.imprimirEBook(*it);
+	}
+	std::cout << '\n';
+
+
+
+	getch();
 	return 0;
 }
